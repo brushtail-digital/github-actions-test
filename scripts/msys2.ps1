b5b11c49e070fc0d7d8d8834865fc6f698c880c9
Jason N <admin@cyderize.org>
2020-06-22 12:38:04 +1000

    Use Start-Process

--- scripts/msys2.ps1
+++ scripts/msys2.ps1
@@ -1,9 +1,12 @@
-# Clear PATH
-$orig_path = $env:PATH;
 $env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -like 'c:\windows*' }) -join ';'
+$env:MSYS2_PATH_TYPE = "inherit"
 
-# Run command
-cmd /S /C "`"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -arch=amd64 -host_arch=amd64 && C:/msys64/msys2_shell.cmd -defterm -no-start -c `"$args`""
+$command = $args
 
-# Restore path
-$env:PATH = $orig_path
+Write-Output "Running command $command in MSYS2 environment"
+
+$cmd_str = "`
+    `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -arch=amd64 -host_arch=amd64 && `
+    C:/msys64/msys2_shell.cmd -defterm -no-start -c `"$command`"";
+
+Start-Process -FilePath "cmd" -Wait -ArgumentList ("/S", "/C", $cmd_str)
