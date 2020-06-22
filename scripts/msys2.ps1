88ed748f6890216367af22dfd954370587ae1791
Jason N <admin@cyderize.org>
2020-06-22 13:02:16 +1000

    Try custom shell

--- scripts/msys2.ps1
+++ scripts/msys2.ps1
@@ -1,12 +1,9 @@
 $env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -like 'c:\windows*' }) -join ';'
 $env:MSYS2_PATH_TYPE = "inherit"
-
-$command = $args
-
-Write-Output "Running command $command in MSYS2 environment"
+$script = $args[0]
 
 $cmd_str = "`
-    `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -arch=amd64 -host_arch=amd64 && `
-    C:/msys64/msys2_shell.cmd -defterm -no-start -c `"$command`"";
+    `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 -host_arch=amd64 && `
+    C:/msys64/msys2_shell.cmd -defterm -no-start $script";
 
-Start-Process -FilePath "cmd" -Wait -ArgumentList ("/S", "/C", $cmd_str)
+Start-Process -FilePath $env:COMSPEC -Wait -ArgumentList ("/S", "/C", $cmd_str)
