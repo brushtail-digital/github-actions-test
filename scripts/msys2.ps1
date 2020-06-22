ac16f2a8ca2609dcf51c496ce8d9fae771d34b59
Jason N <admin@cyderize.org>
2020-06-22 13:46:10 +1000

    Debug

--- scripts/msys2.ps1
+++ scripts/msys2.ps1
@@ -6,6 +6,8 @@ Write-Output "$script"
 
 $cmd_str = "`
     `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 && `
-    C:/msys64/msys2_shell.cmd -defterm -no-start $script";
+    C:/msys64/usr/bin/bash --noprofile --norc -eo pipefail $script";
+
+Write-Output $cmd_str
 
 Start-Process -FilePath $env:COMSPEC -Wait -ArgumentList ("/S", "/C", $cmd_str)
