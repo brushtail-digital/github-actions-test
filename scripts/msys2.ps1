422ffe817438fcfc18180c5d35476ce97e1fb98e
Jason N <admin@cyderize.org>
2020-06-22 14:56:16 +1000

    Test CBC

--- scripts/msys2.ps1
+++
@@ -1,9 +0,0 @@
-$env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -like 'c:\windows*' }) -join ';'
-$env:MSYS2_PATH_TYPE = "inherit"
-
-$script = $args[0]
-$script_location = $(& "C:/msys64/usr/bin/cygpath" "$script")
-
-$cmd_str = "`"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 && C:/msys64/usr/bin/bash --noprofile --norc -eo pipefail `"$script_location`"";
-
-cmd /S /C $cmd_str
