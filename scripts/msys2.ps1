1f9fcc0bfdeffe4fa59ee5b4128f934387427edf
Jason N <admin@cyderize.org>
2020-06-22 13:50:54 +1000

    Update msys2.ps1

--- scripts/msys2.ps1
+++ scripts/msys2.ps1
@@ -4,7 +4,6 @@ $env:MSYS2_PATH_TYPE = "inherit"
 $script = $args[0]
 $script_location = $(& "C:/msys64/usr/bin/cygpath" "$script")
 
-$cmd_str = "`
-    `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 && C:/msys64/usr/bin/bash --noprofile --norc -eo pipefail `"$script_location`"";
+$cmd_str = "`"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 && C:/msys64/usr/bin/bash --noprofile --norc -eo pipefail `"$script_location`"";
 
 & "$env:COMSPEC" "/S" "/C" "$cmd_str"
