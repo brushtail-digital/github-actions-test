f976d664b31bb493184dbd3d311f51c5b668ec4a
Jason N <admin@cyderize.org>
2020-06-22 13:29:42 +1000

    Debug

--- scripts/msys2.ps1
+++ scripts/msys2.ps1
@@ -2,10 +2,10 @@ $env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -like 'c:\windows*' }) -jo
 $env:MSYS2_PATH_TYPE = "inherit"
 $script = $args[0]
 
+Write-Output "$script"
+
 $cmd_str = "`
-    `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 -host_arch=amd64 && `
+    `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 && `
     C:/msys64/msys2_shell.cmd -defterm -no-start $script";
 
 Start-Process -FilePath $env:COMSPEC -Wait -ArgumentList ("/S", "/C", $cmd_str)
-
-Write-Output "Hello, world"
