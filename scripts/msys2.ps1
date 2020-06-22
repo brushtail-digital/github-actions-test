a54167658d6ae9f78d405bf6a5c73e7f2e1ec893
Jason N <admin@cyderize.org>
2020-06-22 13:50:29 +1000

    Debug

--- scripts/msys2.ps1
+++ scripts/msys2.ps1
@@ -1,13 +1,10 @@
 $env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -like 'c:\windows*' }) -join ';'
 $env:MSYS2_PATH_TYPE = "inherit"
-$script = $args[0]
 
-Write-Output "$script"
+$script = $args[0]
+$script_location = $(& "C:/msys64/usr/bin/cygpath" "$script")
 
 $cmd_str = "`
-    `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 && `
-    C:/msys64/usr/bin/bash --noprofile --norc -eo pipefail $script";
+    `"`C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo -arch=amd64 && C:/msys64/usr/bin/bash --noprofile --norc -eo pipefail `"$script_location`"";
 
-Write-Output $cmd_str
-
-Start-Process -FilePath $env:COMSPEC -Wait -ArgumentList ("/S", "/C", $cmd_str)
+& "$env:COMSPEC" "/S" "/C" "$cmd_str"
