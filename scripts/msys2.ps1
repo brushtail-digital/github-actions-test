7ac57069081d35476ee531c7c337072b0f1e1f0b
Jason N <admin@cyderize.org>
2020-06-22 11:41:42 +1000

    Try again

--- scripts/msys2.ps1
+++ scripts/msys2.ps1
@@ -3,8 +3,7 @@ $orig_path = $env:PATH;
 $env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -like 'c:\windows*' }) -join ';'
 
 # Run command
-$command = $args[0]
-cmd /S /C "`"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -arch=amd64 -host_arch=amd64 && C:/msys64/msys2_shell.cmd -defterm -no-start -c `"$command`""
+cmd /S /C "`"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat`" -arch=amd64 -host_arch=amd64 && C:/msys64/msys2_shell.cmd -defterm -no-start -c `"$args`""
 
 # Restore path
 $env:PATH = $orig_path
